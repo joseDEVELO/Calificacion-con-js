695e6efad31102fbcfc738ae6120ce6c8da94bcc
carlosllinas <soportehackcm@gmail.com>
2023-06-17 21:38:51 -0500

    17/06/23

---
+++ interactive-rating-component-main/javascript/index.js
@@ -0,0 +1,9 @@
+/* const numeros = document.querySelector("li");
+
+numeros.forEach(i => {
+    i.addEventListener("click", clicklista);
+    function clicklista(){
+       i.classList.add('active') 
+    }
+});
+ */
